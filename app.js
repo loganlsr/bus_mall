b9647ac265cabd493af08bb60d220d3b26d08886
Logan Rogers <logan.s.rogers@biola.edu>
2016-06-30 10:32:18 -0700

    updated hmtl, CSS, and JS

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 'use strict';
-// JSON.parse(localStorage.data);
 
 function Product(name, path) {
   this.name = name;
@@ -23,8 +22,7 @@ function getRandomIntInclusive() {
 function callProducts() {
   if (localStorage.data) {
     itemArray = JSON.parse(localStorage.data);
-  }
-  else {
+  } else {
     itemArray.push(new Product('Bag', 'images/bag.jpg'));
     itemArray.push(new Product('Banana', 'images/banana.jpg'));
     itemArray.push(new Product('Bathroom', 'images/bathroom.jpg'));
@@ -48,27 +46,19 @@ function callProducts() {
   }
 };
 callProducts();
-// console.log(itemArray);
 
 function randomThreeNum() {
   var one = getRandomIntInclusive();
   var two = getRandomIntInclusive();
   var three = getRandomIntInclusive();
-  // console.log(currentNums);
   while (one === currentNums[0] || one === currentNums[1] || one === currentNums[2]) {
     one = getRandomIntInclusive();
-    // console.log('one hit previous');
-  }
-  while (two === currentNums[0] || two === currentNums[1] || two === currentNums[2] || one === two) {
+  } while (two === currentNums[0] || two === currentNums[1] || two === currentNums[2] || one === two) {
     two = getRandomIntInclusive();
-    // console.log('two hit previous');
-  }
-  while (three === currentNums[0] || three === currentNums[1] || three === currentNums[2] || one === three || two === three) {
+  } while (three === currentNums[0] || three === currentNums[1] || three === currentNums[2] || one === three || two === three) {
     three = getRandomIntInclusive();
-    // console.log('three hit previous');
   }
   currentNums = [one, two, three];
-  // console.log(currentNums);
   return currentNums;
 };
 
@@ -77,33 +67,25 @@ function makeImages() {
   left.src = itemArray[threeNums[0]].path;
   left.alt = itemArray[threeNums[0]].name;
   itemArray[threeNums[0]].tally += 1;
-  // console.log(threeNums[0]);
   middle.src = itemArray[threeNums[1]].path;
   middle.alt = itemArray[threeNums[1]].name;
   itemArray[threeNums[1]].tally += 1;
-  // console.log(threeNums[1]);
+
   right.src = itemArray[threeNums[2]].path;
   right.alt = itemArray[threeNums[2]].name;
   itemArray[threeNums[2]].tally += 1;
-  // console.log(threeNums[2]);
 };
 makeImages();
 
 function handleContainer(event){
-  // console.log(event.target.alt);
   for(var i = 0; i < itemArray.length; i++) {
     if(event.target.alt === itemArray[i].name)
       itemArray[i].click += 1;
-    // console.log(itemArray[i] + 'this is the object we clicked on');
-    // console.log(event.target.alt + 'this is the event target');
-    // console.log(itemArray[i].name + ' has ' + itemArray[i].clicks + ' clicks.');
   }
-
   if (event.target.alt === 'container') {
     alert('You cannot follow instructions to click directly on a contained element. Please do so!');
   } else if (click < 25) {
     click++;
-    // console.log(click);
     makeImages();
   } else if (click === 25) {
     theContainer.removeEventListener('click', handleContainer);
